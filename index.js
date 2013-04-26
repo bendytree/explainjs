88e2b894aef18802cadea6ac97573353aca9bad6
Josh Wright <help@bendytree.com>
2013-04-26 12:07:38 -0500

    added basic repo layoug

---
+++ index.js
@@ -0,0 +1,10 @@
+
+(function(module, module_key){
+    
+    "use strict";
+    
+    module[module_key] = function(js){
+
+    };
+    
+})(module ? module : window, module ? 'exports' : 'explainjs');
--- readme.md
+++ readme.md
@@ -1,1 +1,2 @@
-explain.js
+
+explainjs
