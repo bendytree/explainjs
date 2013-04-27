dc2230759cef0c3b5d0f94ff586f8cfbeb86a5a1
Josh Wright <help@bendytree.com>
2013-04-27 00:57:45 -0500

    added tests

--- index.js
+++ index.js
@@ -1,10 +1,82 @@
 
-(function(module, module_key){
+var markdown = require("node-markdown").Markdown;
+
+module.exports = function(js, callback){
     
     "use strict";
     
-    module[module_key] = function(js){
-
-    };
+    //validate
+    if (typeof js === 'undefined' || !js){
+        callback('invalid source: script should not be blank');
+        return;
+    }
+    if (typeof js !== 'string'){
+        callback('invalid source: script should be a string');
+        return;
+    }
     
-})(module ? module : window, module ? 'exports' : 'explainjs');
+    //replace tabs with spaces
+    js = js.replace(/[\t]/g, '  ');
+    
+    //trim blank lines
+    js = js.replace(/^(\n| )+/, '');
+                
+    // Convert asterisk style comments to whack-whack style.
+    js = js.replace(/\/\*!?((?:[^*]|\*+[^*/])*)\*+\//g, function(match, comment){
+        comment = comment.replace(/^[ *\r\n]+/g, '')
+                         .replace(/^[ *]+/m, '')
+                         .replace(/[ *]+$/mg, '')
+                         .replace(/^/mg, '// ');
+        return comment;
+    });
+    
+    // Now break lines up into sections
+    var sections = [],
+        lines = js.split(/^/m),
+        section = { comments:[], code:[] },
+        breakOnNextComment = false,
+        lastWasComment = true;
+    
+    sections.push(section);
+    
+    for (var i=0; i<lines.length; i++){
+                
+        var line = lines[i],
+            blank = /^[ \r\n]*$/.test(line),
+            comment = (line.match(/^ *\/\/ *(.*)/)||[])[1];
+        
+        if (blank) {
+            if (lastWasComment === false) {
+                section.code.push(''); //retain lines between code blocks
+            }
+            breakOnNextComment = true;
+            continue;
+        }
+        
+        if (typeof comment === 'undefined'){
+            section.code.push(line.replace(/([ \r\n]+$)/g, ''));
+            breakOnNextComment = true;
+            lastWasComment = false;
+        }else{
+            if (breakOnNextComment){
+                breakOnNextComment = false;
+                section = { comments:[], code:[] };
+                sections.push(section);
+            }
+            section.comments.push(comment);
+            lastWasComment = true;
+        }
+    }
+        
+    //convert comments to markdown
+    for (var i=0; i<sections.length; i++){
+        var section = sections[i];
+        
+        var comments = section.comments.join(i===0?'\n\n':'\n');
+        section.comments = markdown(comments).replace(/[\n]+/gm,' ');
+        section.code = section.code.join('\n');
+    }
+    
+    callback(null, {sections:sections});
+};
+
---
+++ test/test.js
@@ -0,0 +1,152 @@
+
+var should = require("should");
+var explainjs = require('../index.js');
+
+
+    it('should handle // comment and code block', function(done){
+        var js = "//hi\ngo();";
+        explainjs(js, function(err, results){
+            results.sections.length.should.equal(1);
+            results.sections[0].comments.should.equal('<p>hi</p>');
+            results.sections[0].code.should.equal('go();');
+            done();
+        });
+    });
+    
+    it('should handle /* */ comment', function(done){
+        var js = "/* hi */\ngo();";
+        explainjs(js, function(err, results){
+            results.sections.length.should.equal(1);
+            results.sections[0].comments.should.equal('<p>hi</p>');
+            results.sections[0].code.should.equal('go();');
+            done();
+        });
+    });
+    
+    it('should handle new line in comment', function(done){
+        var js = "/* \n* hi */\ngo();";
+        explainjs(js, function(err, results){
+            results.sections.length.should.equal(1);
+            results.sections[0].comments.should.equal('<p>hi</p>');
+            results.sections[0].code.should.equal('go();');
+            done();
+        });
+    });
+    
+    it('should handle special characters in comment', function(done){
+        var js = "/* \n* `hi` */\ngo();";
+        explainjs(js, function(err, results){
+            results.sections.length.should.equal(1);
+            results.sections[0].comments.should.equal('<p><code>hi</code></p>');
+            results.sections[0].code.should.equal('go();');
+            done();
+        });
+    });
+    
+
+    it('two comments', function(done){
+        var js = "/* a */\ngo();\n/* b */\ngo2();";
+        explainjs(js, function(err, results){
+            results.sections.length.should.equal(2);
+            results.sections[0].comments.should.equal('<p>a</p>');
+            results.sections[0].code.should.equal('go();');
+            results.sections[1].comments.should.equal('<p>b</p>');
+            results.sections[1].code.should.equal('go2();');
+            done();
+        });
+    });
+    
+    it('first comment should enforce newlines', function(done){
+        var js = "/* a\nb */\ngo();\n/* a\nb */\ngo2();";
+        explainjs(js, function(err, results){
+            results.sections.length.should.equal(2);
+            results.sections[0].comments.should.equal('<p>a</p> <p>b</p>');
+            results.sections[0].code.should.equal('go();');
+            results.sections[1].comments.should.equal('<p>a b</p>');
+            results.sections[1].code.should.equal('go2();');
+            done();
+        });
+    });
+    
+    it('retain spaces preceding code', function(done){
+        var js = "// hi\n  go();";
+        explainjs(js, function(err, results){
+            results.sections.length.should.equal(1);
+            results.sections[0].comments.should.equal('<p>hi</p>');
+            results.sections[0].code.should.equal('  go();');
+            done();
+        });
+    });
+    
+    it('does not put dangling // in code', function(done){
+        var js = "// \ngo();";
+        explainjs(js, function(err, results){
+            results.sections.length.should.equal(1);
+            results.sections[0].comments.should.equal('');
+            results.sections[0].code.should.equal('go();');
+            done();
+        });
+    });
+    
+    it('single linebreak in comment should be ignored', function(done){
+        var js = "//a\na();\n/*a\nb*/\nb();";
+        explainjs(js, function(err, results){
+            results.sections.length.should.equal(2);
+            results.sections[1].comments.should.equal('<p>a b</p>');
+            results.sections[1].code.should.equal('b();');
+            done();
+        });
+    });
+    
+    it('retain double linebreak in comment', function(done){
+        var js = "//a\na();\n/*a\n\nb*/\nb();";
+        explainjs(js, function(err, results){
+            results.sections.length.should.equal(2);
+            results.sections[1].comments.should.equal('<p>a</p> <p>b</p>');
+            results.sections[1].code.should.equal('b();');
+            done();
+        });
+    });
+    
+    it('retain double linebreak in code', function(done){
+        var js = "a();\n\nb();";
+        explainjs(js, function(err, results){
+            results.sections[0].code.should.equal('a();\n\nb();');
+            done();
+        });
+    });
+    
+    it('leading newlines are trimmed', function(done){
+        var js = "\n//hi";
+        explainjs(js, function(err, results){
+            results.sections.length.should.equal(1);
+            results.sections[0].comments.should.equal('<p>hi</p>');
+            done();
+        });
+    });
+    
+    it('======= dont dangle on 2nd comment', function(done){
+        var js = "//a\n\n/* X\n * ====== */";
+        explainjs(js, function(err, results){
+            results.sections.length.should.equal(2);
+            results.sections[1].comments.should.equal('<h1>X</h1>');
+            done();
+        });
+    });
+    
+    it('murders trailing bang on start of multi line comment', function(done){
+        var js = "/*! \n * X */";
+        explainjs(js, function(err, results){
+            results.sections.length.should.equal(1);
+            results.sections[0].comments.should.equal('<p>X</p>');
+            done();
+        });
+    });
+    
+    it('converts tabs to 2 spaces', function(done){
+        var js = "//hi\n\ta();";
+        explainjs(js, function(err, results){
+            results.sections[0].code.should.equal('  a();');
+            done();
+        });
+    });
